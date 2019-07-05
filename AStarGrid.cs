cf6084a95edcf0df9cf79569d9557edf4aa623fa
nicohangaslammi <nico.hangaslammi@gmail.com>
2019-07-05 18:38:35 +0300

    Edit

---
+++ AStarGrid.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace TilemapPathfinding
+{
+    public class AStarGrid : MonoBehaviour
+    {
+        [SerializeField]
+        private bool drawGizmos = true;
+
+        public bool Ready { get; private set; }
+
+        [SerializeField]
+        private Tilemap solidTilemap = null;
+
+        public List<Node> path;
+
+        Node[,] grid;
+
+        float nodeDiameter;
+        int gridSizeX, gridSizeY;
+
+        private void OnEnable()
+        {
+            nodeDiameter = solidTilemap.cellSize.x;
+            gridSizeX = solidTilemap.size.x;
+            gridSizeY = solidTilemap.size.y;
+
+            CreateGrid();
+        }
+
+        public int GridSize
+        {
+            get { return gridSizeX * gridSizeY; }
+        }
+
+        // Create a grid using referenced solid tilemap
+        private void CreateGrid()
+        {
+            grid = new Node[gridSizeX, gridSizeY];
+            Vector2 worldBottomLeft = (Vector2) transform.position - Vector2.right * gridSizeX / 2 - Vector2.up * gridSizeY/2;
+
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                for (int x = 0; x < gridSizeX; x++)
+                {
+                    Vector2 worldPoint = worldBottomLeft
+                    + Vector2.right * (x * nodeDiameter + nodeDiameter / 2)
+                    + Vector2.up * (y * nodeDiameter + nodeDiameter / 2);
+
+                    Vector3Int tilePosition = new Vector3Int(
+                        Mathf.FloorToInt(worldPoint.x),
+                        Mathf.FloorToInt(worldPoint.y),
+                        0
+                    );
+
+                    bool solid = solidTilemap.HasTile(tilePosition);
+
+                    grid[x, y] = new Node(solid, worldPoint, x, y);
+                }
+            }
+
+            Ready = true;
+        }
+
+        // Get a list of surrounding nodes
+        public List<Node> GetNeighbours(Node node)
+        {
+            List<Node> neighbours = new List<Node>();
+
+            for (int y = -1; y <= 1; y++)
+            {
+                for (int x = -1; x <= 1; x++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    if (Mathf.Abs(x) - Mathf.Abs(y) == 0)
+                        continue;
+
+                    int nodeX = node.gridX + x;
+                    int nodeY = node.gridY + y;
+
+                    if (nodeX >= 0 && nodeX < gridSizeX && nodeY >= 0 && nodeY < gridSizeY)
+                    {
+                        neighbours.Add(grid[nodeX, nodeY]);
+                    }
+                }
+            }
+
+            return neighbours;
+        }
+
+        // Convert position to node
+        public Node GetNodeFromWorldPoint(Vector2 worldPosition)
+        {
+            float percentX = (worldPosition.x + gridSizeX / 2) / gridSizeX;
+            float percentY = (worldPosition.y + gridSizeY / 2) / gridSizeY;
+
+            percentX = Mathf.Clamp01(percentX);
+            percentY = Mathf.Clamp01(percentY);
+
+            int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+            int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+            return grid[x, y];
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!drawGizmos) return;
+
+            Gizmos.DrawWireCube(transform.position, new Vector3(gridSizeX, gridSizeY, 1));
+
+            if (grid != null)
+            {
+                foreach (Node node in grid)
+                {
+                    Gizmos.color = node.solid ? Color.red : Color.green;
+                    Gizmos.DrawCube(node.worldPosition, Vector3.one * 0.5f);
+                }
+            }
+
+            if (path != null)
+            {
+                Gizmos.color = Color.blue;
+                foreach (Node node in path)
+                {
+                    if (node.parent != null)
+                    {
+                        Gizmos.DrawLine(node.worldPosition, node.parent.worldPosition);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Heap.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Heap<T> where T : IHeapItem<T> {
+    T[] items;
+    int currentItemCount;
+
+    public Heap(int maxHeapSize)
+    {
+        items = new T[maxHeapSize];
+    }
+
+    public void Add(T item)
+    {
+        item.HeapIndex = currentItemCount;
+        items[currentItemCount] = item;
+        SortUp(item);
+        currentItemCount++;
+    }
+
+    public T RemoveFirst()
+    {
+        T firstItem = items[0];
+        currentItemCount--;
+
+        items[0] = items[currentItemCount];
+        items[0].HeapIndex = 0;
+        SortDown(items[0]);
+        return firstItem;
+    }
+
+    public void UpdateItem(T item)
+    {
+        SortUp(item);
+    }
+
+    public int Count
+    {
+        get { return currentItemCount; }
+    }
+
+    public bool Contains(T item)
+    {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    private void SortDown(T item)
+    {
+        while (true)
+        {
+            int childIndexLeft = item.HeapIndex * 2 + 1;
+            int childIndexRight = item.HeapIndex * 2 + 2;
+            int swapIndex = 0;
+
+            if (childIndexLeft < currentItemCount)
+            {
+                swapIndex = childIndexLeft;
+
+                if (childIndexRight < currentItemCount)
+                {
+                    if (items[childIndexLeft].CompareTo(items[childIndexRight]) < 0)
+                    {
+                        swapIndex = childIndexRight;
+                    }
+                }
+
+                if (item.CompareTo(items[swapIndex]) < 0)
+                {
+                    Swap(item, items[swapIndex]);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void SortUp(T item)
+    {
+        int parentIndex = (item.HeapIndex - 1) / 2;
+
+        while (true)
+        {
+            T parentItem = items[parentIndex];
+            if (item.CompareTo(parentItem) > 0)
+            {
+                Swap(item, parentItem);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    void Swap(T a, T b)
+    {
+        items[a.HeapIndex] = b;
+        items[b.HeapIndex] = a;
+
+        int aIndex = a.HeapIndex;
+        a.HeapIndex = b.HeapIndex;
+        b.HeapIndex = aIndex;
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int HeapIndex
+    {
+        get;
+        set;
+    }
+}
